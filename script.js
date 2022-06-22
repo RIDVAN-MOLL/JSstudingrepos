b00d25356433abc5978adc7aafc54a219d24813b
RIDVAN <rizvan.mallaev@gmail.com>
2022-06-22 12:09:33 +0300

    Очередной первый commit

---
+++ script.js
@@ -0,0 +1,34 @@
+//my first comment. This is one-liner comment
+
+/* First multiline comment
+Second line from multiline comment
+*/
+
+var leftBprderWidth = 1;
+let second = 2;
+const pi =3.14;
+
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj ={}; 
+
+let persone = {
+    name: "john"
+};
+let arr =['plum','png'];
+
+
+//1)
+String(null);
+console.log(1);
+
+//prompt('muha vu ho', 'chog1 dik vy');
+
+let ans = +prompt("ttt", "");
+
+console.log(5 + ans);
